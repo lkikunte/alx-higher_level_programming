8e8b9cc2320f9d797863c1c18a9cb7023e44c21f
lkikunte <lkikunte@gmail.com>
2022-09-22 07:52:35 -0700

    Task 0 corrections

--- 0x02-python-import_modules/0-add.py
+++ 0x02-python-import_modules/0-add.py
@@ -2,4 +2,4 @@
 import add_0
 a = 1
 b = 2
-print(f"{a} + {b} = {add_0.add(1, 2)}")
+print(f"{a} + {b} = {add_0.add(a, b)}")
---
+++ 0x02-python-import_modules/1-calculation.py
@@ -0,0 +1,5 @@
+#!/usr/bin/python3
+import calculator_1
+a = 10
+b = 5
+pri
